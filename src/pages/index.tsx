40461add11551e396045470133fe2ddefe16ff19
Sho Sakai <ropitaru88@gmail.com>
2022-06-27 01:12:12 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from "@chakra-ui/react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Heading>Hello</Heading>
+    </Box>
+  );
+};
+
+export default Home;
